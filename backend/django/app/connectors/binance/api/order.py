130e5bd7830310ee7741f7500cc48c5a8cde7628
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2025-10-25 09:23:08 +0000

    refactor order

--- backend/django/app/connectors/binance/api/order.py
+++ backend/django/app/connectors/binance/api/order.py
@@ -1,3 +1,46 @@
-def new_order():
-    print("Placing new order...")
-    pass
+import os
+import logging
+from dotenv import load_dotenv
+
+from binance_sdk_derivatives_trading_usds_futures.derivatives_trading_usds_futures import (
+    DerivativesTradingUsdsFutures,
+    ConfigurationRestAPI,
+    DERIVATIVES_TRADING_USDS_FUTURES_REST_API_PROD_URL,
+)
+from binance_sdk_derivatives_trading_usds_futures.rest_api.models import (
+    NewOrderSideEnum,
+    NewOrderTimeInForceEnum,
+)
+
+load_dotenv()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+configuration_rest_api = ConfigurationRestAPI(
+    api_key=os.environ.get('API_KEY_BINANCE'),
+    api_secret=os.environ.get('API_SECRET_BINANCE'),
+    base_path=os.getenv(
+        "BASE_PATH", DERIVATIVES_TRADING_USDS_FUTURES_REST_API_PROD_URL
+    ),
+)
+
+client = DerivativesTradingUsdsFutures(config_rest_api=configuration_rest_api)
+
+def new_order(price):
+    try:
+        response = client.rest_api.new_order(
+            symbol="paxgusdt",
+            quantity=0.002,
+            price=price,
+            side=NewOrderSideEnum["BUY"].value,
+            type="LIMIT",
+            time_in_force=NewOrderTimeInForceEnum["GTC"].value,
+        )
+
+        rate_limits = response.rate_limits
+        logging.info(f"new_order() rate limits: {rate_limits}")
+
+        data = response.data()
+        logging.info(f"new_order() response: {data}")
+    except Exception as e:
+        logging.error(f"new_order() error: {e}")
--- backend/django/app/connectors/binance/management/commands/run_binance_new_order.py
+++ backend/django/app/connectors/binance/management/commands/run_binance_new_order.py
@@ -1,63 +1,20 @@
-import os
 import logging
 import time
 import asyncio
 import threading
-from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
+import app.connectors.binance.api.ticker as ticker_stream
+from app.connectors.binance.api.order import new_order
 
-from binance_sdk_derivatives_trading_usds_futures.derivatives_trading_usds_futures import (
-    DerivativesTradingUsdsFutures,
-    ConfigurationRestAPI,
-    DERIVATIVES_TRADING_USDS_FUTURES_REST_API_PROD_URL,
-)
-from binance_sdk_derivatives_trading_usds_futures.rest_api.models import (
-    NewOrderSideEnum,
-    NewOrderTimeInForceEnum,
-)
-
-from . import run_binance_individual_symbol_ticker as ticker_stream
-
-load_dotenv()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
-configuration_rest_api = ConfigurationRestAPI(
-    api_key=os.environ.get('API_KEY_BINANCE'),
-    api_secret=os.environ.get('API_SECRET_BINANCE'),
-    base_path=os.getenv(
-        "BASE_PATH", DERIVATIVES_TRADING_USDS_FUTURES_REST_API_PROD_URL
-    ),
-)
-
-client = DerivativesTradingUsdsFutures(config_rest_api=configuration_rest_api)
-
-def new_order(price):
-    try:
-        response = client.rest_api.new_order(
-            symbol="paxgusdt",
-            quantity=0.002,
-            price=price,
-            side=NewOrderSideEnum["BUY"].value,
-            type="LIMIT",
-            time_in_force=NewOrderTimeInForceEnum["GTC"].value,
-        )
-
-        rate_limits = response.rate_limits
-        logging.info(f"new_order() rate limits: {rate_limits}")
-
-        data = response.data()
-        logging.info(f"new_order() response: {data}")
-    except Exception as e:
-        logging.error(f"new_order() error: {e}")
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logging.info(self.style.SUCCESS("Connecting to Binance WebSocket for ticker stream..."))
 
         websocket_thread = threading.Thread(target=asyncio.run,
-                                            args=(ticker_stream.individual_symbol_book_ticker_streams(),),
+                                            args=(ticker_stream.subscribe_symbol_ticker(),),
                                             daemon=True)
         websocket_thread.start()
 
