86edb9e764c5fd4e1ad7a3d86b6c7b38f4337ac3
suphakorn <supakornhomnan@gmail.com>
2025-12-07 12:24:45 +0000

    add rest api to get summary bot data to show on the frontend site

--- backend/django/app/ui_web/urls.py
+++ backend/django/app/ui_web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.health_check_page, name = 'health_check_page'),
     path('pause/', views.handle_pause_position_sync, name= 'pause_position_sync')
+    path('summary/', views.get_arbitrage_summary, name= 'get_arbitrage_summary')
 ]
--- backend/django/app/ui_web/views.py
+++ backend/django/app/ui_web/views.py
@@ -78,6 +78,76 @@ def health_check_page(request):
     }
     return render(request, 'health-check.html', context)
 
+@api_view(['GET'])
+def get_arbitrage_summary(request):
+    try:
+        binance_symbol = 'PAXGUSDT'
+        mt5_symbol = 'XAUUSD'
+        ratio = 1 # edit to 100 after changing contract_size in position_sync
+
+        redis_conn = get_redis_connection()
+        binance_key = f"position:{binance_symbol}"
+        mt5_key = f"position: {mt5_symbol}"
+        pause_position_key = f"position_sync_paused_flag"
+
+        # Retrieve cache data
+        data = redis_conn.get(binance_key)
+        mt5_data = redis_conn.get(mt5_key)
+        pause_position = 'Active'
+
+        result = prepare_json(data)
+        mt5_result = prepare_json(mt5_data)
+
+        binance_action = 'SHORT'
+        mt5_action  = 'SHORT'
+        pairStatus = 'Warning'
+
+        binance_size = float(result['positionAmt'])
+        mt5_size = float(mt5_result['positionAmt'])
+        unrealizes = [float(result['unRealizedProfit']), float(mt5_result['unRealizedProfit'])]
+        netExpose = (binance_size * ratio) + mt5_size
+        netExposeAction = 'Safe'
+
+        if netExpose != 0:
+            netExposeAction = 'Unsafe'
+
+        # Condition defined
+        if binance_size > 0:
+            binance_action = 'LONG'
+        if binance_size == 0:
+            binance_action = 'None'
+        if mt5_size > 0:
+            mt5_action = 'LONG'
+        if mt5_size == 0:
+            mt5_action = 'None'
+        if (binance_size + mt5_size) == 0:
+            pairStatus = 'Complete'
+        if redis_conn.get(pause_position_key):
+            pause_position = 'Pause'
+
+
+        data = {
+            'binanceMarkPrice': result['markPrice'],
+            'mt5MarkPrice': mt5_result['markPrice'],
+            'spread': float(result['markPrice']) - float(mt5_result['markPrice']),
+            'pairStatus': pairStatus,
+            'binanceSize': binance_size,
+            'binanceAction': binance_action,
+            'mt5Size': mt5_size,
+            'netExpose': netExpose,
+            'netExposeAction': netExposeAction,
+            'mt5Action': mt5_action,
+            'unrealizedBinance': sum(unrealizes),
+            'pausePositionSync': pause_position,
+            'time_update_mt5': mt5_result['time_update'],
+            'time_update_binance': result['updateTime']
+        }
+
+        return Response({ message: "Successful retrieved summary data", data: data }, status=status.HTTP_200_OK)
+    except Exception as e:
+        logger.error(f"Pausing bot error: {e}")
+        return Response({"message": f"Server error: {e}", "is_paused": None}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 @api_view(['POST'])
 def handle_pause_position_sync(request):
     try:
