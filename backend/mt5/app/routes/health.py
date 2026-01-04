dca33987e6c441fd635545deb85b2e7e3eb3e706
suphakorn <supakornhomnan@gmail.com>
2026-01-04 10:03:32 +0000

    write api to get mt5 account information

--- backend/mt5/app/routes/health.py
+++ backend/mt5/app/routes/health.py
@@ -36,3 +36,39 @@ def health_check():
         "mt5_connected": mt5 is not None,
         "mt5_initialized": initialized
     }), 200
+
+@health_bp.route('/account_info')
+@swag_from({
+    'tags': ['Health'],
+    'responses': {
+        200: {
+            'description': 'Get account information successful',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'login': {'type': 'string'},
+                    'name': {'type': 'string'},
+                    'server': {'type': 'string'},
+                    'status': {'type': 'string'},
+                }
+            }
+        }
+    }
+})
+def get_account_info():
+    try:
+        if not mt5.initialize():
+            print("initialize() failed, error code =",mt5.last_error())
+        account_info = mt5.account_info()._asdict()
+
+        return jsonify({
+            'status': 'successful',
+            'login': account_info['login'],
+            'server': account_info['server'],
+            'name': account_info['name']
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'reason': str(e)
+        }), 500
