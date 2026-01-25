263da13866abf597c2f087fc5dc8ac166ba7a7a1
suphakorn <supakornhomnan@gmail.com>
2026-01-25 08:15:20 +0000

    add: silver as a new pair trading

--- backend/control-api/app/constants/config.py
+++ backend/control-api/app/constants/config.py
@@ -1,4 +1,5 @@
 PAIRS = [
     {'binance': 'PAXGUSDT', 'mt5': 'XAUUSD'}, 
-    {'binance': 'XAUUSDT', 'mt5': 'XAUUSD'}
+    {'binance': 'XAUUSDT', 'mt5': 'XAUUSD'},
+    {'binance': 'XAGUSDT', 'mt5': 'XAGUSD'}
     ]
