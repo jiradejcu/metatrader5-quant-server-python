b746de0b76abe3e45c1a89b3d6bd7f203bf55505
suphakorn <supakornhomnan@gmail.com>
2026-02-14 14:37:55 +0000

    fix: add more handle conditional logics to show expected result

--- backend/control-api/app/events/services/quant.py
+++ backend/control-api/app/events/services/quant.py
@@ -34,8 +34,8 @@ def get_arbitrage_summary():
         logger.info(f"Get redis key: position: {binance_symbol} success")
         mt5_result = prepare_json(redis_conn.get(mt5_key), position_data_default)
         logger.info(f"Get redis key: position: {mt5_symbol} success")
+        
         pause_position = 'Active'
-
         binance_action = 'SHORT'
         mt5_action = 'SHORT'
         pairStatus = 'Warning'
@@ -43,29 +43,7 @@ def get_arbitrage_summary():
         binance_size = float(result.get('positionAmt', 0))
         mt5_size = float(mt5_result.get('positionAmt', 0))
         unrealizes = [float(result.get('unRealizedProfit', 0)), float(mt5_result.get('unRealizedProfit', 0))]
-        netExpose = binance_size + (mt5_size * ratio) # 1 PAXG = 0.01 XAU
-        netExposeAction = 'Safe'
-
-        # handle floating point issue
-        epsilon = 1e-12 
-        if abs(netExpose) < epsilon:
-            netExpose = 0
-
-        if netExpose != 0:
-            netExposeAction = 'Unsafe'
-
-        if binance_size > 0:
-            binance_action = 'LONG'
-        elif binance_size == 0:
-            binance_action = 'None'
-
-        if mt5_size > 0:
-            mt5_action = 'LONG'
-        elif mt5_size == 0:
-            mt5_action = 'None'
-
-        if netExpose == 0:
-            pairStatus = 'Complete'
+        
 
         if redis_conn.get(pause_position_key):
             pause_position = 'Pause' 
@@ -76,20 +54,15 @@ def get_arbitrage_summary():
             'mt5MarkPrice': float(mt5_result.get('markPrice', 0)),
             'binanceEntry': float(result.get('entryPrice', 0)),
             'mt5Entry': float(mt5_result.get('entryPrice',0)),
-            'spread': float(result.get('markPrice', 0)) - float(mt5_result.get('markPrice', 0)),
-            'pairStatus': pairStatus,
             'binanceSize': binance_size,
-            'binanceAction': binance_action,
             'mt5Size': mt5_size,
-            'netExpose': netExpose,
-            'netExposeAction': netExposeAction,
-            'mt5Action': mt5_action,
-            'unrealizedBinance': sum(unrealizes),
             'pausePositionSync': pause_position,
             'time_update_mt5': now,
             'time_update_binance': now,
             'binanceSymbol': binance_symbol,
             'mt5Symbol': mt5_symbol,
+            'binance_unrealized_profit': float(result.get('unRealizedProfit', 0)),
+            'mt5_unrealized_profit': float(mt5_result.get('unRealizedProfit', 0))
         }
 
         # Fill missing data from the latest response (Clean data)
@@ -105,8 +78,36 @@ def get_arbitrage_summary():
             response_data['binanceSize'] = latest_response_data['binanceSize']
         if latest_response_data != {} and response_data['mt5Size'] == 0 and latest_response_data['mt5Size'] != 0:
             response_data['mt5Size'] = latest_response_data['mt5Size']
+        if latest_response_data != {} and response_data['binance_unrealized_profit'] == 0 and latest_response_data['unrealizedBinance'] != 0:
+            response_data['binance_unrealized_profit'] = latest_response_data['binance_unrealized_profit']
+        if latest_response_data != {} and response_data['mt5_unrealized_profit'] == 0 and latest_response_data['mt5_unrealized_profit'] != 0:
+            response_data['mt5_unrealized_profit'] = latest_response_data['mt5_unrealized_profit']
 
+        response_data['netExpose'] = response_data['binanceSize'] + (response_data['mt5Size'] * ratio) # 1 PAXG = 0.01 XAU
         response_data['spread'] = response_data['binanceMarkPrice'] - response_data['mt5MarkPrice']
+        response_data['unrealizedBinance'] = response_data['binance_unrealized_profit'] + response_data['mt5_unrealized_profit']
+        response_data['netExposeAction'] = 'Safe'
+
+        # handle floating point issue
+        epsilon = 1e-12 
+        if abs(response_data['netExpose']) < epsilon:
+            response_data['netExpose'] = 0
+
+        if response_data['netExpose'] != 0:
+            response_data['netExposeAction'] = 'Unsafe'
+
+        if response_data['binanceSize'] > 0:
+            response_data['binanceAction'] = 'LONG'
+        elif response_data['binanceSize'] == 0:
+            response_data['binanceAction'] = 'None'
+
+        if response_data['mt5Size'] > 0:
+            response_data['mt5Action'] = 'LONG'
+        elif response_data['mt5Size'] == 0:
+            response_data['mt5Action'] = 'None'
+
+        if response_data['netExpose'] == 0:
+            response_data['pairStatus'] = 'Complete'    
         
         # Update the latest response data for future reference
         latest_response_data = response_data
