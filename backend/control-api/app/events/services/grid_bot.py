3fb835aaf22e747e59be623a70a6611df990703f
suphakorn <supakornhomnan@gmail.com>
2026-02-22 09:36:28 +0000

    add close long & short parameters

--- backend/control-api/app/events/services/grid_bot.py
+++ backend/control-api/app/events/services/grid_bot.py
@@ -13,7 +13,7 @@ PAIR_INDEX = int(os.getenv('PAIR_INDEX'))
 logger = logging.getLogger(__name__)
 
 binance_data_default = {'positionAmt': 0, 'markPrice': 0, 'unRealizedProfit': 0, 'time_update': None, 'updateTime': None}
-grid_data_default = {'upper_diff': 0.0, 'lower_diff': 0.0, 'max_position_size': 0.0, 'order_size': 0.0 } 
+grid_data_default = {'upper_diff': 0.0, 'lower_diff': 0.0, 'max_position_size': 0.0, 'order_size': 0.0, 'close_long': 0.0, 'close_short': 0.0 } 
 
 def get_grid_parameters_data():
         redis_conn = get_redis_connection()
@@ -31,10 +31,12 @@ def get_grid_parameters_data():
 
                 data = {
                 "upper_diff": float(grid_data.get('upper_diff', 0.0)),
-                "lower_diff": float(grid_data.get('lower_dfiff', 0.0)),
+                "lower_diff": float(grid_data.get('lower_diff', 0.0)),
                 "max_position_size": float(grid_data.get('max_position_size', 0.0)),
                 "order_size": float(grid_data.get('order_size', 0.0)),
                 "mark_price": float(result.get('markPrice', 0.0)),
+                "close_long": float(grid_data.get('close_long', 0.0)),
+                "close_short": float(grid_data.get('close_short', 0.0)),
                 "time": now
                 }
 
