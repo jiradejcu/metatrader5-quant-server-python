d32fdbf7357ffedd496808971437673f341538f8
suphakorn <supakornhomnan@gmail.com>
2026-02-22 09:56:11 +0000

    add instance for dev site

--- frontend/src/hooks/all-bot.ts
+++ frontend/src/hooks/all-bot.ts
@@ -3,10 +3,12 @@ export const useAllBots = () => {
     const botUrl = import.meta.env.VITE_API_BASE_URL;
     const botUrl2 = import.meta.env.VITE_API_BASE_URL_2;
     const botUrl3 = import.meta.env.VITE_API_BASE_URL_3
+    const botUrlDev = import.meta.env.VITE_API_BASE_URL_DEV
 
     return {
         botUrl,
         botUrl2,
-        botUrl3
+        botUrl3,
+        botUrlDev
     }
 }
