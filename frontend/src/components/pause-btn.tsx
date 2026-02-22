bb6365b97120f245bba09af4b88e962ae5b13880
suphakorn <supakornhomnan@gmail.com>
2026-02-22 09:56:48 +0000

    adjust margin

--- frontend/src/components/pause-btn.tsx
+++ frontend/src/components/pause-btn.tsx
@@ -32,7 +32,7 @@ function PausePositionBtn({ url }: IDokcerAPIBtnProps) {
 
   // Derive all UI states directly from the mutation object
   let btnText = 'Pause position sync toggle';
-  let btnClasses = "mb-4 font-semibold py-2 px-4 rounded transition duration-200 border ";
+  let btnClasses = "mt-4 font-semibold py-2 px-4 rounded transition duration-200 border ";
   let message = "";
   let messageClass = "mt-2 h-5 text-sm font-semibold ";
 
