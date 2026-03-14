163a691ccb35f5efa1b4f5caf0c331e5cc7cfc5b
suphakorn <supakornhomnan@gmail.com>
2026-03-14 14:10:58 +0000

    add state to keep required attributes to decide open orders

---
+++ backend/django/app/quant/algorithms/arbitrage/state.py
@@ -0,0 +1,13 @@
+import threading
+
+# define variable states here
+placing_order_state = {
+    "order_id": None,
+    "status": None,
+    "is_clean": True,
+    "fill_pct": 0,
+    "side": None
+}
+
+# Lock to ensure thread-safety when reading/writing to variable
+state_lock = threading.Lock()
