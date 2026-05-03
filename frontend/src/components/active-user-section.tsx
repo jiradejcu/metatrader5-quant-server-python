ea70334903746bc10596f2c97baf70dcbd94fbc0
suphakorn <supakornhomnan@gmail.com>
2026-05-03 05:50:35 +0000

    remove api key tool tip

--- frontend/src/components/active-user-section.tsx
+++ frontend/src/components/active-user-section.tsx
@@ -17,16 +17,7 @@ export const ActiveUserSection = (arg: ICardSection) => {
     }
     return (
         <>
-            <p className="text-sm text-gray-600 dark:text-gray-400">Binance Holder: <span className="relative group cursor-pointer font-mono text-blue-600 dark:text-blue-400 font-bold ml-2">{activeUser?.binance_account_name}
-                <span
-                    className="absolute bottom-full left-1/2
-                    transform -translate-x-1/2 mb-2
-                    w-max px-2 py-1 text-sm text-white
-                    bg-gray-700 rounded shadow-lg
-                    opacity-0 group-hover:opacity-100">
-                    API Key: {activeUser?.binance_key}
-                </span>
-            </span></p>
+            <p className="text-sm text-gray-600 dark:text-gray-400">Binance Holder: <span className="font-mono text-blue-600 dark:text-blue-400 font-bold ml-2">{activeUser?.binance_account_name}</span></p>
 
             <p className="text-sm text-gray-600 dark:text-gray-400 mt-1">MT5 Account: <span className="font-mono text-green-600 dark:text-green-400 font-bold ml-2">{activeUser?.name} [{activeUser?.login}] | Server {activeUser?.server} </span></p>
         </>
